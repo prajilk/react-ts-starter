2a479e87f9fbc12438850659ecc0be5880b874e6
Prajil K <prajilk20017@gmail.com>
2024-04-29 09:47:04 +0530

    React Typecript starter code

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
+import queryConfig from "./config/react-query";
+import PageLoading from "./components/common/page-loading";
+import Error404 from "./components/common/error404";
+import Home from "./pages/home";
+
+const queryClient = new QueryClient({
+    defaultOptions: queryConfig,
+});
+
+function App() {
+    return (
+        <Suspense fallback={<PageLoading />}>
+            <div className="App">
+                <QueryClientProvider client={queryClient}>
+                    <NextUIProvider>
+                        <Routes>
+                            <Route path="*" element={<Error404 />} />
+                            <Route path="/" element={<Home />} />
+                        </Routes>
+                    </NextUIProvider>
+                </QueryClientProvider>
+            </div>
+            <Toaster position="top-right" />
+        </Suspense>
+    );
+}
+
+export default App;
