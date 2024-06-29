daf144b26e073ccafce9ebc2b2b19b299dc52d45
Prajil K <prajilk20017@gmail.com>
2024-06-29 18:33:03 +0530

    renamed kebab case to camel case

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,30 @@ import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "sonner";
 import queryConfig from "./config/react-query";
-import PageLoading from "./components/common/page-loading";
-import Error404 from "./components/common/error404";
-import Home from "./pages/home";
+import PageLoading from "./components/common/PageLoading";
+import Error404 from "./components/common/Error404";
+import Home from "./pages/Home";
 
 const queryClient = new QueryClient({
-    defaultOptions: queryConfig,
+  defaultOptions: queryConfig,
 });
 
 function App() {
-    return (
-        <Suspense fallback={<PageLoading />}>
-            <div className="App">
-                <QueryClientProvider client={queryClient}>
-                    <NextUIProvider>
-                        <Routes>
-                            <Route path="*" element={<Error404 />} />
-                            <Route path="/" element={<Home />} />
-                        </Routes>
-                    </NextUIProvider>
-                </QueryClientProvider>
-            </div>
-            <Toaster position="top-right" />
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<PageLoading />}>
+      <div className="App">
+        <QueryClientProvider client={queryClient}>
+          <NextUIProvider>
+            <Routes>
+              <Route path="*" element={<Error404 />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </NextUIProvider>
+        </QueryClientProvider>
+      </div>
+      <Toaster position="top-right" />
+    </Suspense>
+  );
 }
 
 export default App;
